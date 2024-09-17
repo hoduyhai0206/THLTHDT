14314e452449afdaf878106de37823d5be6673c2
hoduyhai0206 <hoduyhai02062005@gmail.com>
2024-09-18 03:22:11 +0700

    init project

---
+++ src/Bai2.java
@@ -0,0 +1,12 @@
+public class Bai2 {
+    public static void main(String[] args) {
+        String ten = "Hồ Duy Hải";
+        int tuoi = 20;
+        double chieucao = 1.7;
+        boolean thichlaptrinh = true;
+        System.out.println("Tên: " + ten);
+        System.out.println("Tuổi: " + tuoi);
+        System.out.println("Chiều cao: " + chieucao + "m");
+        System.out.println("Có thích lập trình không? " + (thichlaptrinh ? "Có" : "Không"));
+    }
+}
